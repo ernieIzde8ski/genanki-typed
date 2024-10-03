572291d31593fee21a69a008cf210dae37742d49
Ernest Izdebski <44341687+ernieIzde8ski@users.noreply.github.com>
2024-10-02 22:58:15 -0400

    Export `__all__` in root `__init__.py`
    The `__all__` attribute achieves two things:
    
    1. Tells type checkers which classes, functions, and variables are "public".
    2. Configures what is imported with star ("*") imports.

--- genanki/__init__.py
+++ genanki/__init__.py
@@ -1,3 +1,18 @@
+__all__ = [
+  "BASIC_AND_REVERSED_CARD_MODEL",
+  "BASIC_MODEL",
+  "BASIC_OPTIONAL_REVERSED_CARD_MODEL",
+  "BASIC_TYPE_IN_THE_ANSWER_MODEL",
+  "CLOZE_MODEL",
+  "Card",
+  "Deck",
+  "Model",
+  "Note",
+  "Package",
+  "guid_for",
+  "__version__",
+]
+
 from .builtin_models import (
   BASIC_AND_REVERSED_CARD_MODEL,
   BASIC_MODEL,
